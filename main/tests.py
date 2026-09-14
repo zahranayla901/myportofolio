7cd6dee008f1a848c2c3a0f0538fec21a2aaa88f
Zahra Nayla Azfa <zahra.nayla51@ui.ac.id>
2026-09-14 21:41:47 +0700

    docs: add tugas 2 to README.md

--- main/tests.py
+++ main/tests.py
@@ -46,7 +46,7 @@ class MainTest(TestCase):
         Experience.objects.all().delete()
         response = self.client.get(reverse("main:show_experience"))
 
-        self.assertContains(response, "Belum ada pengalaman yang ditambahkan.")
+        self.assertContains(response, "No projects have been added yet.")
 
     def test_completed_experience(self):
         self.experience.ended_at = timezone.now()
@@ -94,4 +94,4 @@ class ProjectTest(TestCase):
         Project.objects.all().delete()
         response = self.client.get(reverse("main:show_experience"))
 
-        self.assertContains(response, "Belum ada pengalaman yang ditambahkan.")
+        self.assertContains(response, "No projects have been added yet.")
