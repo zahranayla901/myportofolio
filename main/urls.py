0bddb999e482a7617a3e1793d97f5b0891a72322
Zahra Nayla Azfa <zahra.nayla51@ui.ac.id>
2026-09-16 13:25:18 +0700

    feat: Tutorial 03: Form and Data Delivery

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_project
+from main.views import delete_project, get_project_json, show_main, show_experience, show_project, create_project
 
 app_name = "main"
 
@@ -8,4 +8,7 @@ urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
     path("project/", show_project, name="show_project"),
+    path("project/add/", create_project, name="create_project"),
+    path("api/project/", get_project_json, name="get_project_json"),
+    path("project/<uuid:project_id>/delete/",delete_project,name="delete_project"),
 ]
