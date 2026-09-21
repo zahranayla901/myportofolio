8c0f2e2d3120a4d85176e6833eb0aa2bc18ea57c
Zahra Nayla Azfa <zahra.nayla51@ui.ac.id>
2026-09-21 15:58:14 +0700

    feat: add ExperienceForm for experience CRUD

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm, TextInput, Textarea, URLInput
 
-from main.models import Project
+from main.models import Project, Experience
 
 class ProjectForm(ModelForm):
     class Meta:
@@ -47,6 +47,62 @@ class ProjectForm(ModelForm):
             "thumbnail": TextInput(
                 attrs={
                     "placeholder": "https://drive.google.com/thumbnail?id=FILE_ID&sz=w1000",
+                }
+            ),
+        }
+
+class ExperienceForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = [
+            "title",
+            "description",
+            "category",
+            "thumbnail",
+            "started_at",
+            "ended_at",
+        ]
+
+        labels = {
+            "title": "Experience Title",
+            "description": "Description",
+            "category": "Choose Category",
+            "thumbnail": "Exp Image Path",
+            "started_at": "Started at",
+            "ended_at": "Ended at",
+        }
+
+        widgets = {
+            "title": TextInput(
+                attrs={
+                    "placeholder": "COMPFEST 18 Creative Staff",
+                    "maxlength": 255,
+                }
+            ),
+            "description": Textarea(
+                attrs={
+                    "placeholder": "I Sketch",
+                    "rows": 3,
+                }
+            ),
+            "category": TextInput(
+                attrs={
+                    "placeholder": "volunteer",
+                }
+            ),
+            "thumbnail": TextInput(
+                attrs={
+                    "placeholder": "https://drive.google.com/thumbnail?id=FILE_ID&sz=w1000",
+                }
+            ),
+            "started_at": TextInput(
+                attrs={
+                    "placeholder": "dd/mm/yyyy",
+                }
+            ),
+            "ended_at": TextInput(
+                attrs={
+                    "placeholder": "dd/mm/yyyy",
                 }
             ),
         }
